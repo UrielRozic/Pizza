5ea65d467f3f9bec1841db995c6146d8674df756
URIEL EIAL ROZIC <46025935@ort>
2022-04-05 09:04:43 -0300

    progreso

--- Pizzas.API-master/Services/PizzasService.cs
+++ Pizzas.API-master/Services/PizzasService.cs
@@ -9,7 +9,7 @@ using Dapper;
 using System.Data.SqlClient; 
        
        
-        public static List<Pizza> SelectAll (){
+        /*public static List<Pizza> SelectAll (){
             List<Pizza> pizzas = new List<Pizza>();
             string sql = "SELECT * FROM Pizzas";
             using(SqlConnection Bd =  new SqlConnection(_connectionString)){
@@ -47,6 +47,50 @@ using System.Data.SqlClient;
                Registro = Bd.Execute(sql, new {p = Id});
            }
            return Registro;
+        }*/
+
+        public static List<Pizza> GetAll() {
+            string sqlQuery;    
+            List<Pizza> returnList;
+
+            returnList = new List<Pizza>();
+            using (SqlConnection db = new SqlConnection(CONNECTION_STRING)) {      
+                sqlQuery = "SELECT * FROM Pizzas";      
+                returnList  = db.Query<Pizza>(sqlQuery).ToList();
+                }    
+                
+            return returnList;
+        }
+
+        public static Pizza GetById(int id) {
+            string  sqlQuery;    
+            Pizza returnEntity = null;
+
+            sqlQuery = "SELECT Id, Nombre, LibreGluten, Importe, Descripcion FROM Pizzas ";    
+            sqlQuery = "WHERE Id = @idPizza";
+            using (SqlConnection db = new SqlConnection(CONNECTION_STRING)) {        
+                returnEntity  = db.QueryFirstOrDefault<Pizza>(sqlQuery, new { idPizza = id });
+            }    
+            return returnEntity;
+        }
+
+        public static int Insert(Pizza pizza) {   
+            string  sqlQuery;    
+            int     intRowsAffected = 0;           
+            sqlQuery  = "INSERT INTO Pizzas ("; 
+            sqlQuery += "Nombre, LibreGluten, Importe, Descripcion";    
+            sqlQuery += ") VALUES (";sqlQuery += "@nombre, @libreGluten, @importe, @descripcion"; 
+            sqlQuery += ")";
+            using (SqlConnection db = new SqlConnection(CONNECTION_STRING)) {
+                intRowsAffected = db.Execute(sqlQuery, new { 
+                    nombre      = pizza.Nombre,   
+                        libreGluten = pizza.LibreGluten,  
+                                importe     = pizza.Importe, 
+                                  descripcion = pizza.Descripcion
+                                  } );    }    
+            
+            return intRowsAffected; 
+            
         }
 
         public static int TestTryCatch(){
